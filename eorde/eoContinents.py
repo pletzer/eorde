61d456634159fde110d2607ccb82a3dbfdc58c69
Alex Pletzer <alexander@gokliya.net>
2019-12-09 17:44:15 +1300

    + continents

---
+++ eorde/eoContinents.py
@@ -0,0 +1,30 @@
+import vtk
+
+
+class Continents:
+
+    def __init__(self, level=0):
+
+        self.continents = vtk.vtkEarthSource()
+        self.mapper = vtk.vtkPolyDataMapper()
+        self.actor = vtk.vtkActor()
+
+        # connect
+        self.actor.SetMapper(self.mapper)
+        self.mapper.SetInputConnection(self.continents.GetOutputPort())
+
+    def getActor(self):
+        return self.actor
+
+###############################################################################
+
+def test():
+    from eoScene import Scene
+    c = Continents(level=1)
+    s = Scene()
+    s.addPipelines([c])
+    s.setBackground(0.9, 0.9, 0.9)
+    s.show()
+
+if __name__ == '__main__':
+    test()
